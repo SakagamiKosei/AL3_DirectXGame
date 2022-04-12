70a34ec5ba604dfb791ee2d900573186eda43bff
sakagamikosei <k021g1111@g.neec.ac.jp>
2022-04-12 16:03:50 +0900

    ウィンドウタイトル変更

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -15,7 +15,6 @@
 /// ゲームシーン
 /// </summary>
 class GameScene {
-
   public: // メンバ関数
 	/// <summary>
 	/// コンストクラタ
@@ -47,7 +46,22 @@ class GameScene {
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
-
+	// テクスチャハンドル
+	uint32_t  textureHandle_ = 0;
+	// サウンドデータハンドル
+	uint32_t soundDataHandle_ = 0;
+	// 音声再生ハンドル
+	uint32_t voiceHandle_ = 0;
+	// 値を表示したい変数
+	int32_t value_ = 0;
+	// スプライトの生成
+	Sprite* sprite_ = nullptr;
+	// 3Dモデル
+	Model* model_ = nullptr;
+	// ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	// ビュープロジェクション
+	ViewProjection viewProjection_;
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
