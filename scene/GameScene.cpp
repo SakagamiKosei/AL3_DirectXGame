c70b17612226b5d9d3214b3ac3d284f5e45cd86d
sakagamikosei <k021g1111@g.neec.ac.jp>
2022-04-19 01:12:08 +0900

    02_02_ビュー変換行列

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,7 +6,10 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete sprite_;
+	delete model_;
+}
 
 void GameScene::Initialize() {
 
@@ -14,9 +17,54 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+	// ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
+
+	// 3Dモデルの生成
+	model_ = Model::Create();
+	// X,Y,Z 方向のスケーリングを設定
+	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
+	//// X,Y,Z　軸回りの回転角を設定
+	worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
+	// X,Y,Z　軸周りの平行移動を設定
+	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
+
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	// ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+
+	// サウンドデータの読み込み
+	soundDataHandle_ = audio_->LoadWave("se_sad03.wav");
+	// 音声再生
+	audio_->PlayWave(soundDataHandle_);
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	// スペースキーを押した瞬間
+	if (input_->TriggerKey(DIK_SPACE)) {
+		// 音声停止
+		audio_->StopWave(voiceHandle_);
+	}
+	std::string translationDebug =
+	  std::string("translation:(") + std::to_string(worldTransform_.translation_.x) +
+	  std::string(",") + std::to_string(worldTransform_.translation_.y) + std::string(",") +
+	  std::to_string(worldTransform_.translation_.z) + 
+	std::string(")");
+	std::string rotationDebug =
+	  std::string("rotation:(") + std::to_string(worldTransform_.rotation_.x) +
+	  std::string(",") + std::to_string(worldTransform_.rotation_.y) + std::string(",") +
+	  std::to_string(worldTransform_.rotation_.z) + std::string(")");
+	std::string scaleDebug = std::string("scale:(") +
+	                            std::to_string(worldTransform_.scale_.x) + std::string(",") +
+	                            std::to_string(worldTransform_.scale_.y) + std::string(",") +
+	                            std::to_string(worldTransform_.scale_.z) + std::string(")");
+	// デバックテキストの表示
+	debugText_->Print(translationDebug, 50, 50, 1.0f);
+	debugText_->Print(rotationDebug, 50, 70, 1.0f);
+	debugText_->Print(scaleDebug, 50, 90, 1.0f);
+}
 
 void GameScene::Draw() {
 
@@ -44,6 +92,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	// 3Dモデル描画
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
