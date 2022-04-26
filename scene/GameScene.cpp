d4e0a06d7116756ffc611d1840c415eba2fbab8d
sakagamikosei <k021g1111@g.neec.ac.jp>
2022-04-26 13:59:49 +0900

    02_01_ex1

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,7 +1,6 @@
 ﻿#include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
-#include <random>
 
 using namespace DirectX;
 
@@ -13,187 +12,90 @@ GameScene::~GameScene() {
 }
 
 void GameScene::Initialize() {
-
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
 	// ファイル名を指定してテクスチャを読み込む
 	textureHandle_ = TextureManager::Load("mario.jpg");
-
+	// スプライトの生成
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	// ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	// サウンドデータの読み込み
+	soundDataHandle_ = audio_->LoadWave("se_sad03.wav");
+	// 音声生成
+	audio_->PlayWave(soundDataHandle_);
+	// 音声再生
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
 	// 3Dモデルの生成
 	model_ = Model::Create();
-	worldTransform_[PartId::Root].scale_ = {0.6f, 1.0f, 1.0f};
-	// キャラクターの大元
-	worldTransform_[PartId::Root].Initialize();
-
-	// 脊椎
-	worldTransform_[PartId::Spine].translation_ = {0.0f, 0.0f, 0.0f};
-	worldTransform_[PartId::Spine].parent_ = &worldTransform_[PartId::Root];
-	worldTransform_[PartId::Spine].Initialize();
-	// ---------------------------------------------------------------------
-	// 上半身
-	// ---------------------------------------------------------------------
-	// 胸
-	worldTransform_[PartId::Chest].Initialize();
-	worldTransform_[PartId::Chest].parent_ = &worldTransform_[PartId::Spine];
-	worldTransform_[PartId::Chest].translation_ = {0.0f, 5.0f, 0.0f};
-	// 頭
-	worldTransform_[PartId::Head].Initialize();
-	worldTransform_[PartId::Head].parent_ = &worldTransform_[PartId::Chest];
-	worldTransform_[PartId::Head].translation_ = {0.0f, 3.0f, 0.0f};
-	// 左腕
-	worldTransform_[PartId::ArmL].Initialize();
-	worldTransform_[PartId::ArmL].parent_ = &worldTransform_[PartId::Chest];
-	worldTransform_[PartId::ArmL].translation_ = {-3.0f,0.0f, 0.0f};
-	// 右腕
-	worldTransform_[PartId::ArmR].Initialize();
-	worldTransform_[PartId::ArmR].parent_ = &worldTransform_[PartId::Chest];
-	worldTransform_[PartId::ArmR].translation_ = {3.0f, 0.0f, 0};
-
-	// ----------------------------------------------------------------------
-	// 下半身
-	// ----------------------------------------------------------------------
-	// 尻
-	worldTransform_[PartId::Hip].Initialize();
-	worldTransform_[PartId::Hip].parent_ = &worldTransform_[PartId::Spine];
-	worldTransform_[PartId::Hip].translation_ = {0.0f, 2.0f, 0};
-	// 左足
-	worldTransform_[PartId::LegL].Initialize();
-	worldTransform_[PartId::LegL].parent_ = &worldTransform_[PartId::Hip];
-	worldTransform_[PartId::LegL].translation_ = {-3.0f, -3.0f, 0};
-	// 右足
-	worldTransform_[PartId::LegR].Initialize();
-	worldTransform_[PartId::LegR].parent_ = &worldTransform_[PartId::Hip];
-	worldTransform_[PartId::LegR].translation_ = {3.0f, -3.0f, 0};
-
-	// カメラ視点座標を設定
-	viewProjection_.eye = {0, 0, -50};
-	// カメラ注視点座標を設定
-	viewProjection_.target = {0, 0, 0};
-	// カメラ上方向ベクトルを設定(右上45度指定)
-	viewProjection_.up = {0.0f, 1.0f, 0.0f};
-	// カメラ垂直方向視野角を設定
-	viewProjection_.fovAngleY = XMConvertToRadians(45.0f);
-	// アスペクト比を設定
-	viewProjection_.aspectRatio = 1.0f;
-	// ニアクリップ距離を設定
-	viewProjection_.nearZ = 0.10f;
-	// ファークリップ距離を設定
-	viewProjection_.farZ = 53.0f;
+	//// X,Y,Z 方向のスケーリングを設定
+	// worldTransform_.scale_ = {5.0f, 1.0f, 1.0f};
+	//// X,Y,Z 軸周りの回転角を設定
+	// worldTransform_.rotation_ = {0.0f,XM_PI/4.0f,0.0f};
+	//  X,Y,Z 軸回りの回転角を設定
+	worldTransform_.rotation_ = {0.0f, XMConvertToRadians(45.0f), 0.0f};
+	//// X,Y,Z 軸回りの平行移動を設定
+	// worldTransform_.translation_ = {0.0f, 10.0f, 0.0f};
+	//  X,Y,Z 方向のスケーリングを設定
+	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
+	// X,Y,Z　軸回りの回転角を設定
+	worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
+	// X,Y,Z 軸回りの平行移動を設定
+	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
 	// ビュープロジェクションの初期化
 	viewProjection_.Initialize();
 }
 
 void GameScene::Update() {
-	
-	// デバック用表示
-	debugText_->SetPos(50, 50);
-	debugText_->Printf("eye:(%f,%f,%f)", viewProjection_.eye.x, viewProjection_.eye.y,viewProjection_.eye.z);
-
-	// デバック用表示
-    debugText_->SetPos(50, 70);
-	debugText_->Printf("target:(%f,%f,%f)", viewProjection_.target.x, viewProjection_.target.y,viewProjection_.target.z);
-	
-    // デバック用表示
-	debugText_->SetPos(50, 90);
-	debugText_->Printf("up:(%f,%f,%f)", viewProjection_.up.x, viewProjection_.up.y, viewProjection_.up.z);
-
-	// デバック用表示
-	debugText_->SetPos(50, 110);
-	debugText_->Printf("forAngleY(Degree):%f", XMConvertToDegrees(viewProjection_.fovAngleY));
-
-	// デバック用表示
-	debugText_->SetPos(50, 130);
-	debugText_->Printf("nearZ:%f", viewProjection_.nearZ);
-
-	// キャラクター移動処理
-	{
-		// キャラクターの移動ベクトル
-		XMFLOAT3 move = {0, 0, 0};
-
-		// キャラクターの移動速さ
-		const float kCharacterSpeed = 0.1f;
-
-		// 押した方向で移動ベクトルを変更
-		if (input_->PushKey(DIK_LEFT)) {
-			move = {-kCharacterSpeed, 0, 0};
-		} else if (input_->PushKey(DIK_RIGHT)) {
-			move = {kCharacterSpeed, 0, 0};
-		}
-
-		// 注視点移動(ベクトルの加算)
-		worldTransform_[PartId::Root].translation_.x += move.x;
-		worldTransform_[PartId::Root].translation_.y += move.y;
-		worldTransform_[PartId::Root].translation_.z += move.z;
-
-		// デバック用表示
-		debugText_->SetPos(50, 150);
-		debugText_->Printf(
-		  "Root:(%f,%f,%f)", worldTransform_[PartId::Root].translation_.x,
-		  worldTransform_[PartId::Root].translation_.y,
-		  worldTransform_[PartId::Root].translation_.z);
+	// スプライトの今の座標を取得
+	XMFLOAT2 position = sprite_->GetPosition();
+	// 座標を{2,0}移動
+	position.x += 2.0f;
+	position.y += 1.0f;
+	// 移動した座標をスプライトに反映
+	sprite_->SetPosition(position);
+	// スペースキーを押した瞬間
+	if (input_->TriggerKey(DIK_SPACE)) {
+		// 音声停止
+		audio_->StopWave(voiceHandle_);
 	}
+	//// デバックテキストの表示
+	// debugText_->Print("Kaizokuou ni oreha naru.", 50, 50, 1.0f);
+	//  書式指定付き表示
+	debugText_->SetPos(50, 70);
+	// debugText_->Printf("year:%d", 2001);
+	//  変数の値をインクリメント
+	value_++;
+	// 値を含んだ文字列
+	std::string translationDebug =
+	  std::string("translation:(") + std::to_string(worldTransform_.translation_.x) +
+	  std::string(",") + std::to_string(worldTransform_.translation_.y) + std::string(",") +
+	  std::to_string(worldTransform_.translation_.z) + std::string(")");
 
-	//worldTransform_[0].UpdateMatrix();
-	//worldTransform_[1].UpdateMatrix();
+	std::string rotationDebug = std::string("rotation:(") +
+	                            std::to_string(worldTransform_.rotation_.x) + std::string(",") +
+	                            std::to_string(worldTransform_.rotation_.y) + std::string(",") +
+	                            std::to_string(worldTransform_.rotation_.z) + std::string(")");
 
-	// 大元から順に更新していく
-	worldTransform_[PartId::Root].UpdateMatrix();
-	worldTransform_[PartId::Spine].UpdateMatrix();
-	worldTransform_[PartId::Chest].UpdateMatrix();
-	worldTransform_[PartId::Head].UpdateMatrix();
-	worldTransform_[PartId::ArmL].UpdateMatrix();
-	worldTransform_[PartId::ArmR].UpdateMatrix();
-	worldTransform_[PartId::Hip].UpdateMatrix();
-	worldTransform_[PartId::LegL].UpdateMatrix();
-	worldTransform_[PartId::LegR].UpdateMatrix();
+	std::string scaleDebug = std::string("scale:(") + std::to_string(worldTransform_.scale_.x) +
+	                         std::string(",") + std::to_string(worldTransform_.scale_.y) +
+	                         std::string(",") + std::to_string(worldTransform_.scale_.z) +
+	                         std::string(")");
 
-	// 上半身回転処理
-	{
-		// 上半身の回転速さ[ラジアン/frame]
-		const float kChestRotSpeed = 0.05f;
-
-		// 押した方向で移動ベクトルを変更
-		if (input_->PushKey(DIK_U)) {
-			worldTransform_[PartId::Chest].rotation_.y -= kChestRotSpeed;
-		} else if (input_->PushKey(DIK_I)) {
-			worldTransform_[PartId::Chest].rotation_.y += kChestRotSpeed;
-		} 
-	}
-
-	// 下半身回転処理
-	{
-		// 下半身の回転速さ[ラジアン/frame]
-		const float kHipRotSpeed = 0.05f;
-
-		// 押した方向で移動ベクトルを変更
-		if (input_->PushKey(DIK_J)) {
-			worldTransform_[PartId::Hip].rotation_.y -= kHipRotSpeed;
-		} else if (input_->PushKey(DIK_K)) {
-			worldTransform_[PartId::Hip].rotation_.y += kHipRotSpeed;
-		} 
-	}
+	// デバックテキストの表示
+	debugText_->Print(translationDebug, 50, 50, 1.0f);
+	debugText_->Print(rotationDebug, 50, 70, 1.0f);
+	debugText_->Print(scaleDebug, 50, 90, 1.0f);
 }
 
 void GameScene::Draw() {
 
 	// コマンドリストの取得
 	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
-
-#pragma region 背景スプライト描画
-	// 背景スプライト描画前処理
-	Sprite::PreDraw(commandList);
-
-	/// <summary>
-	/// ここに背景スプライトの描画処理を追加できる
-	/// </summary>
-
-	// スプライト描画後処理
-	Sprite::PostDraw();
-	// 深度バッファクリア
-	dxCommon_->ClearDepthBuffer();
-#pragma endregion
 
 #pragma region 3Dオブジェクト描画
 	// 3Dオブジェクト描画前処理
@@ -203,22 +105,24 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 	// 3Dモデル描画
-	//model_->Draw(worldTransform_[0], viewProjection_, textureHandle_);
-	//model_->Draw(worldTransform_[1], viewProjection_, textureHandle_);
-
-	// キャラクターの描画
-	model_->Draw(worldTransform_[PartId::Chest], viewProjection_, textureHandle_);
-	model_->Draw(worldTransform_[PartId::Head], viewProjection_, textureHandle_);
-	model_->Draw(worldTransform_[PartId::ArmL], viewProjection_, textureHandle_);
-	model_->Draw(worldTransform_[PartId::ArmR], viewProjection_, textureHandle_);
-	model_->Draw(worldTransform_[PartId::Hip], viewProjection_, textureHandle_);
-	model_->Draw(worldTransform_[PartId::LegL], viewProjection_, textureHandle_);
-	model_->Draw(worldTransform_[PartId::LegR], viewProjection_, textureHandle_);
-
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
 
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+	/*sprite_->Draw();*/
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
 #pragma region 前景スプライト描画
 	// 前景スプライト描画前処理
 	Sprite::PreDraw(commandList);
@@ -229,7 +133,6 @@ void GameScene::Draw() {
 
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
-	//
 	// スプライト描画後処理
 	Sprite::PostDraw();
 
